beb38f812d5454ea9aacb77381026d0ebb26252c
AoZhang <superao@163.com>
2024-06-03 18:31:51 +0800

    update

--- src/osgsatellite/osgsatellite.cpp
+++ src/osgsatellite/osgsatellite.cpp
@@ -24,7 +24,20 @@
 #include <osg/Uniform>
 #include <osgDB/Registry>
 #include <osgDB/ReadFile>
+#include <osgDB/WriteFile>
+#include <osg/Image>
+#include <osg/NodeVisitor>
+#include <osg/Drawable>
+#include <osgViewer/ViewerEventHandlers>
 
+// exposure time 1s, d_length 1 mm^2
+const double g_CCDphotons = 19100;
+// satellite
+const double g_E_sun = 573; // Irradiance of the sun measured in[W/m^2]
+const double g_sun_app_mag = -26.7; // sun apparent magnitude
+// CCD gray parameters
+const double n_ec = 45000;    // CCD per pixel max photons
+const double n_e = n_ec / 256;    // photons per gray scale
 
 std::string readShaderFile(const std::string& filePath) {
     std::ifstream shaderFile(filePath);
@@ -41,8 +54,138 @@ std::string readShaderFile(const std::string& filePath) {
     }
 }
 
+// we consider center point (xc,yc) = (0,0)
+double simplifiedGaussianFunc(const double x, const double y, const double xc, const double yc, const double sigma)
+{
+    double xDis = x - xc;
+    double yDis = y - yc;
+    return std::exp(-(xDis * xDis + yDis * yDis) / (2 * sigma * sigma));
+}
+
+double getEnergyProportion(const double x_start, const double y_start, const double x_end, const double y_end, const double steps, const double sigma)
+{
+    // gaussian spread center coords are (0,0)
+    double hx = (x_end - x_start) / steps; // x 方向步长
+    double hy = (y_end - y_start) / steps; // y 方向步长
+    double xc = 0.0;
+    double yc = 0.0;
+    double sum = 0.0;
+    for (uint32_t i = 0; i < steps; ++i)
+    {
+        for (uint32_t j = 0; j < steps; ++j)
+        {
+            double x0 = x_start + i * hx; // 当前子区间左边界的 x 值
+            double x1 = x_start + (i + 1) * hx; // 当前子区间右边界的 x 值
+            double y0 = y_start + j * hy; // 当前子区间下边界的 y 值
+            double y1 = y_start + (j + 1) * hy; // 当前子区间上边界的 y 值
+
+            sum += (simplifiedGaussianFunc(x0, y0, xc, yc, sigma) +
+                simplifiedGaussianFunc(x1, y0, xc, yc, sigma) +
+                simplifiedGaussianFunc(x0, y1, xc, yc, sigma) +
+                simplifiedGaussianFunc(x1, y1, xc, yc, sigma)) / 4.0 * hx * hy;
+        }
+    }
+    sum = 1 / (2 * osg::PI * sigma * sigma) * sum;
+    return sum;
+}
+
+osg::Texture2D* genPSFTexture(unsigned int pixelSize, const double sigma)
+{
+    osg::Image* image = new osg::Image;
+    image->allocateImage(pixelSize, pixelSize, 1, GL_LUMINANCE, GL_UNSIGNED_BYTE);
+    unsigned char* data = image->data();
+    double sum = 0.0;
+    double centerIndex = pixelSize / 2;
+    for (unsigned int i = 0; i < pixelSize; i++)
+    {
+        for (unsigned int j = 0; j < pixelSize; j++)
+        {
+            double energyProportion = getEnergyProportion((double)i - centerIndex, (double)j - centerIndex, 
+                (double)i - centerIndex + 1, (double)j - centerIndex + 1, 100, sigma);
+            sum += energyProportion;
+            //energyProportion = 182745.87074774309 * energyProportion / n_e / 256.0;
+            energyProportion = 487315350.22900116 * energyProportion / n_e / 256.0;
+            data[i * pixelSize + j] = (unsigned char)(255.99f * std::min(energyProportion, 1.0));
+        }
+    }
+
+    osg::Texture2D* texture = new osg::Texture2D;
+    texture->setImage(image);
+    texture->setInternalFormat(GL_RGB);
+    texture->setBorderColor(osg::Vec4(0.0, 0.0, 0.0, 0.0));
+    texture->setFilter(osg::Texture2D::MIN_FILTER, osg::Texture2D::LINEAR_MIPMAP_LINEAR);
+    texture->setFilter(osg::Texture2D::MAG_FILTER, osg::Texture2D::LINEAR);
+    texture->setWrap(osg::Texture2D::WRAP_S, osg::Texture2D::CLAMP_TO_BORDER);
+    texture->setWrap(osg::Texture2D::WRAP_T, osg::Texture2D::CLAMP_TO_BORDER);
+
+    osgDB::writeImageFile(*image, std::string("test.png"));
+    return texture;
+}
+
+
+
+// get photons from star magitude on CCD
+double getPhotonsFromMagnitude(double mv, double d_len, double t_in)
+{
+    double n_pe = g_CCDphotons * 1 / std::pow(2.5, mv) * t_in * osg::PI * (d_len / 2) * (d_len / 2) * 1e6;
+    return n_pe;
+}
+
+/**
+ * @brief
+ * @param r -> the distance between the target and the sensor [m]
+ * @param rho -> effective reflectance
+ * @param area -> effective reflective area [m^2]
+ * @return
+ */
+double computeIrradianceOfSatellite(const double r, const double rho, const double area)
+{
+    double E = g_E_sun / (osg::PI * r * r) * rho * area;
+    return E;
+}
+
+double irradianceToApparentMagnitude(const double e)
+{
+    double m = 2.5 * std::log10(g_E_sun / e) + g_sun_app_mag;
+    return m;
+}
+
+class DistanceUpdateCallback : public osg::Drawable::UpdateCallback
+{
+public:
+    DistanceUpdateCallback(osg::Camera* camera, osg::ref_ptr<osg::Geometry> geo)
+        : m_camera(camera), m_geo(geo) 
+    {
+        m_lastViewMatrix = m_camera->getViewMatrix();
+    }
+
+    virtual void update(osg::NodeVisitor* nv, osg::Drawable*)
+    {
+        osg::Matrixd curMatrix = m_camera->getViewMatrix();
+        if (m_lastViewMatrix != curMatrix)
+        {
+            std::cout << "Moving!" << std::endl;
+        }
+        m_lastViewMatrix = curMatrix;
+    }
+protected:
+    osg::ref_ptr<osg::Camera> m_camera;
+    osg::ref_ptr<osg::Geometry> m_geo;
+    osg::Matrixd m_lastViewMatrix;
+    float _thresholdDistance = 10.0f; // Adjust the threshold distance as needed
+};
+
 int main()
 {
+    osg::ref_ptr<osg::Texture2D> PSFTex = genPSFTexture(12, 0.85);
+    // calculate irradiance of satellite 
+    //double E = computeIrradianceOfSatellite(648610.0, 0.25, 0.290);
+    double E = computeIrradianceOfSatellite(20440.0, 0.25, 0.800);
+    double target_mag = irradianceToApparentMagnitude(E);
+    double d_len = 0.03; // unit m
+    double t_exposure = 5;    // unit s
+    double n_pe = getPhotonsFromMagnitude(target_mag, d_len, t_exposure);
+
     osgViewer::Viewer* viewer = new osgViewer::Viewer;
     osg::ref_ptr<osg::Group> rootGroup = new osg::Group;
     osg::ref_ptr<osg::Group> SatelliteGroup = new osg::Group;// 前(x)右(y)下(z)，转序就是zyx(321)
@@ -67,10 +210,11 @@ int main()
     vertices->push_back(osg::Vec3(5.0f, 0.0f, 0.0f)); // Point position
     vertices->push_back(osg::Vec3(10.0f, 0.0f, 0.0f)); // Point position
     geom->setVertexArray(vertices.get());
-    osg::ref_ptr<osg::Vec4Array> colorArray = new osg::Vec4Array;
+    geom->dirtyBound();
+    /*osg::ref_ptr<osg::Vec4Array> colorArray = new osg::Vec4Array;
     colorArray->push_back(osg::Vec4(0.0f, 255.0f, 0.0f, 1.0f));
     colorArray->push_back(osg::Vec4(0.0f, 255.0f, 0.0f, 0.0f));
-    geom->setColorArray(colorArray.get(), osg::Array::BIND_PER_VERTEX);
+    geom->setColorArray(colorArray.get(), osg::Array::BIND_PER_VERTEX);*/
 
     // Set up the shader program
     osg::ref_ptr<osg::Program> program = new osg::Program;
@@ -82,9 +226,12 @@ int main()
     geom->getOrCreateStateSet()->setAttributeAndModes(program.get());
     // Get the width of the window
     //unsigned int width = viewer->getCamera()->getViewport()->width();
-
+    geom->getOrCreateStateSet()->setTextureAttributeAndModes(0, PSFTex.get(), osg::StateAttribute::ON);
     geom->getOrCreateStateSet()->addUniform(new osg::Uniform("window_width_half", 1280)); // for temporary
     geom->getOrCreateStateSet()->addUniform(new osg::Uniform("window_height_half", 720)); // for temporary
+    geom->getOrCreateStateSet()->addUniform(new osg::Uniform("tex", 0)); // for temporary
+    geom->getOrCreateStateSet()->addUniform(new osg::Uniform("n_pe", n_pe));
+    geom->getOrCreateStateSet()->addUniform(new osg::Uniform("n_e", n_e));
     geom->getOrCreateStateSet()->setMode(GL_POINT_SMOOTH, osg::StateAttribute::OFF | osg::StateAttribute::PROTECTED);
     geom->getOrCreateStateSet()->setMode(GL_POINT_SPRITE, osg::StateAttribute::ON | osg::StateAttribute::PROTECTED);
     geom->getOrCreateStateSet()->setAttributeAndModes(new osg::BlendFunc(), osg::StateAttribute::ON);
@@ -94,7 +241,7 @@ int main()
     osg::ref_ptr<osg::Point> pointAttrib = new osg::Point;
     pointAttrib->setSize(12.0); // Set the size of the point
     geom->getOrCreateStateSet()->setAttributeAndModes(pointAttrib.get());
-
+    geom->setUpdateCallback(new DistanceUpdateCallback(viewer->getCamera(), geom.get()));
     satellitePointGeode->addDrawable(geom.get());
     satellitePointGeode->getOrCreateStateSet()->setRenderBinDetails(1, "RenderBin");
     satellitePointGeode->getOrCreateStateSet()->setAttributeAndModes(new osg::Depth(), osg::StateAttribute::OFF);
@@ -102,6 +249,7 @@ int main()
     satellitePosMt->addChild(satellitePointGeode.get());
     rootGroup->addChild(SatelliteGroup.get());
     viewer->setSceneData(rootGroup.get());
+    viewer->addEventHandler(new osgViewer::StatsHandler());
     return viewer->run();
 }
 
--- src/osgsatellite/satellitepoint.fs
+++ src/osgsatellite/satellitepoint.fs
@@ -1,16 +1,28 @@
-﻿varying int window_pos_x;
-varying int window_pos_y;
+﻿// version OpenGL 2.1 & GLSL 1.2, 
+varying float window_pos_x;
+varying float window_pos_y;
 
-varying float n_pe;
-varying int ptSize;
-varying int highlightType;
+uniform float n_pe;
+varying float ptSize;
+varying float highlightType;
 uniform float n_e;  // 每1个灰度值对应的电子数
+uniform sampler2D tex;
 void main()
 {
-    gl_FragColor = vec4(gl_FragCoord.xyz, 1.0);//vec4(1.0, 0.0, 0.0, 1.0); // Set the color of the point (red)
-
-    if (gl_FragCoord.x <= 1.0)
+    // gl_FragCoord holds the window relative coordinates x, y, z, and 1/w values for the fragment
+    gl_FragColor.a = 1.0;
+    gl_FragColor.r = 0.0;
+    if (window_pos_x - gl_FragCoord.x <1e-3)
     {
-        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+        gl_FragColor.r = 1.0;
     }
+  
+    gl_FragColor.g = 1.0;
+    float temp = texture(tex, gl_PointCoord).x;
+    //temp = n_pe * temp / n_e / 256.0; 
+    gl_FragColor = vec4(temp, temp, temp, 1.0);
+    // if (temp < 0.02)
+    // {
+    //     gl_FragColor.a = 0.0;
+    // }
 }
